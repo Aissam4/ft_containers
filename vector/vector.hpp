10d98203edd31e782443418cb4f9dbe9effe91ed
Aissam4 <aissambarchil75@gmail.com>
2022-04-13 15:07:55 +0000

    (vector) init vector files

---
+++ vector/vector.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abarchil <abarchil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 14:55:10 by abarchil          #+#    #+#             */
+/*   Updated: 2022/04/13 15:07:03 by abarchil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __VECTOR_HPP
+#define __VECTOR_HPP
+
+# include <string>
+# include <iostream>
+# include <algorithm>
+# include <limits>
+
+namespace ft
+{
+    template <typename T>
+    class vector
+    {
+        public:
+        
+        private:
+    };
+    
+}
+#endif
