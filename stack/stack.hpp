37c1e64a8a399b50b8d6723052fe0ed0076e8af4
Aissam4 <aissambarchil75@gmail.com>
2022-05-03 22:13:40 +0000

    (linux : stack) add all requirment files

---
+++ stack/stack.hpp
@@ -0,0 +1,19 @@
+#ifndef __STACK_HPP__
+#define __STACK_HPP__
+#include <stack.hpp>
+#include <iostream>
+#include <algorithm>
+
+namespace ft
+{
+    template <class T>
+    class stack
+    {
+        private:
+
+        public:
+            stack();
+            ~stack();
+    };
+}
+#endif
