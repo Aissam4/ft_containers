3155a48587707d7646bd0dca0e81fe292c8bf3a1
Aissvm4 <aissambarchil75@gmail.com>
2022-06-12 14:11:54 +0100

    (chore : utils) : implement pair
    std::pair is a class template that provides a way to store two heterogeneous objects as a single unit

--- utils/pair.hpp
+++ utils/pair.hpp
@@ -2,22 +2,69 @@
 #define __PAIR_HPP__
 namespace ft{
 
-template<class T1, class T2>
-class pair{
-	public:
-		T1	first;
-		T2	second;
-	pair( void ){
-		first();
-		second();
+	template<class T1, class T2>
+	class pair{
+		public:
+			T1	first;
+			T2	second;
+		pair( void ){
+			first();
+			second();
+		}
+		pair(const pair &obj){
+			*this  = obj;
+		}
+		pair(const T1 &obj1, const T2 &obj2){
+			first(obj1);
+			second(obj2);
+		}
+		template<class X, classs Y>
+		pair(pair<X, Y> &obj){
+			first(obj.first);
+			second(obj.second);
+		}
+		~pair( void ){}
+		pair	&operator=(const pair &obj){
+			if (this != &obj){
+				this->first = obj.first;
+				this->second = obj.second;
+			}
+			return (*this);
+		}
+	};
+	template <class T1, class T2>
+    bool operator==(const pair<T1, T2> &p1, const pair<T1, T2> &p2){
+        return (p1.first == p2.first && p1.second == p2.second);
+    }
+
+    template <class T1, class T2>
+    bool operator!=(const pair<T1, T2> &p1, const pair<T1, T2> &p2){
+        return !(p1 == p2);
+    }
+
+    template <class T1, class T2>
+    bool operator<(const pair<T1, T2> &p1, const pair<T1, T2> &p2){
+        return (p1.first < p2.first || (p1.first == p2.first &&  p1.second < p2.second));
+    }
+
+    template <class T1, class T2>
+    bool operator<=(const pair<T1, T2> &p1, const pair<T1, T2> &p2){
+        return (p1.first <= p2.first);
+    }
+
+    template <class T1, class T2>
+    bool operator>(const pair<T1, T2> &p1, const pair<T1, T2> &p2){
+        return !(p1 <= p2);
+    }
+
+    template <class T1, class T2>
+    bool operator>=(const pair<T1, T2> &p1, const pair<T1, T2> &p2){
+        return !(p1 < p2);
+    }
+	template<class T1, class T2>
+	pair<T1, T2> make_pair(T1 x, T2 y)
+	{
+		return (pair<T1, T2>(x, y));
 	}
-	pair(const pair &obj){
-		*this  = obj;
-	}
-	pair(const T1 &obj1, const T2 &obj2){
-		first(obj1);
-		second(obj2);
-	}
-}
 }
 #endif
