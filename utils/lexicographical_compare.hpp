a5c7f9d162138f235d76db8c2220c4192030e69b
Aissam Barchil <abarchil@e3r5p4.1337.ma>
2022-06-18 17:22:12 +0100

    (chore : lexicographicl compare) finish implementation

--- utils/lexicographical_compare.hpp
+++ utils/lexicographical_compare.hpp
@@ -6,7 +6,7 @@
 /*   By: abarchil <abarchil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 15:30:19 by abarchil          #+#    #+#             */
-/*   Updated: 2022/06/18 15:32:11 by abarchil         ###   ########.fr       */
+/*   Updated: 2022/06/18 16:44:17 by abarchil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,36 @@
 namespace ft{
 	
 	template< class InputIt1, class InputIt2 >
-	bool lexicographical_compare( InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2 );
+	bool lexicographical_compare( InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2 )
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1) 
+				return false;
+			if (*first1 < *first2)
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
 
 	template< class InputIt1, class InputIt2, class Compare >
 	bool lexicographical_compare( InputIt1 first1, InputIt1 last1,
 								InputIt2 first2, InputIt2 last2,
-                              	Compare comp );
+                              	Compare comp )
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || comp(*first2, *first1)) 
+				return false;
+			else if (comp(*first1, *first2))
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
 }
+
 #endif
