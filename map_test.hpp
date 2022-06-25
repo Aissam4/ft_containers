ce6194d84a1852b24c95021fa7300d108c37c0dd
Aissam Barchil <abarchil@e3r2p1.1337.ma>
2022-06-25 18:24:34 +0100

    (tests) add map tests

--- map_test.hpp
+++ map_test.hpp
@@ -5,12 +5,14 @@ void	map_test( void ){
 	std::map<int, int> std_map;
 	for (size_t i = 0; i < 25; i++)
 	{
-		ft_map.insert(ft::pair<int, int> (rand() %200, i));
-		std_map.insert(std::pair<int, int> (rand() %200, i));
+		int num = rand() % 300;
+		ft_map.insert(ft::pair<int, int> (num, i));
+		std_map.insert(std::pair<int, int> (num, i));
 	}
 	std::cout << "ft_map test\n" << std::endl;
-	for (size_t i = 0; i < ft_map.size(); i++){
-		// std::cout << ft_map[i] << std::endl;
-	}
-
+	for (ft::map<int, int>::iterator it = ft_map.begin() ; it != ft_map.end(); it++)
+		std::cout << "Map Key : " << it->first << " | Map Value : " << it->second << std::endl;
+	std::cout << "std_map test\n" << std::endl;
+	for (std::map<int, int>::iterator it = std_map.begin() ; it != --std_map.end(); it++)
+		std::cout << "Map Key : " << it->first << " | Map Value : " << it->second << std::endl;
 }
